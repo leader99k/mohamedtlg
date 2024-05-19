eb3dd3bb349d78cf7cc3f887cb02fd56448e6774
leader99k <166257451+leader99k@users.noreply.github.com>
2024-05-19 17:13:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import telebot,time
 from telebot import types
 from gatet import Tele
 import os
-token = '6877380816:AAE3TkKV4ZoAbTqW0sbaEnYNzmlYjMhs7-E'
+token = '6954681464:AAFHsxNSIPjQ2HeLC-DWXmCGKJSJ8_M-CmM'
 bot=telebot.TeleBot(token,parse_mode="HTML")
-subscriber =1267649387
+subscriber =6637296556
 @bot.message_handler(commands=["start"])
 def start(message):
-	if not str(message.chat.id) == '1267649387':
+	if not str(message.chat.id) == '6637296556':
 		bot.reply_to(message, "You cannot use the bot to contact developers to purchase a bot subscription @NOTBASRA")
 		return
 	bot.reply_to(message,"Send the file now \n ارسل الملف الان")
 @bot.message_handler(content_types=["document"])
 def main(message):
-	if not str(message.chat.id) == '1267649387':
+	if not str(message.chat.id) == '6637296556':
 		bot.reply_to(message, "You cannot use the bot to contact developers to purchase a bot subscription @NOTBASRA")
 		return
 	dd = 0
